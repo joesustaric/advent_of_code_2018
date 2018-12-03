5563b451196db5d0f387e86a44cdd6d04bcc428a
Joe Sustaric <joe.sustaric@envato.com>
2018-12-03 18:33:38 +1100

    performance comments and rename func

--- ruby/01/01_solution_pt2.rb
+++ ruby/01/01_solution_pt2.rb
@@ -21,8 +21,8 @@ def generate_input_as_hash(input_file_location)
   result
 end
 
-def find_duplicate_freq(freq_set, input_deltas, starting_freq)
-  current_freq = starting_freq
+def find_duplicate_freq(freq_set, input_deltas, previous_freq)
+  current_freq = previous_freq
 
   input_deltas.each do |delta|
     r = ResultFreq.new(
@@ -41,3 +41,15 @@ end
 
 
 puts find_duplicate_freq(Set.new, generate_input_as_hash('input.txt'), 0)
+
+# bundle exec ruby 01_solution_pt2.rb  0.61s user 0.12s system 97% cpu 0.752 total
+# ~/m/a/r/01 ❯❯❯ time bundle exec ruby 01_solution_pt2.rb                                                                                                                                              master
+# bundle exec ruby 01_solution_pt2.rb  0.64s user 0.13s system 96% cpu 0.796 total
+# ~/m/a/r/01 ❯❯❯ time bundle exec ruby 01_solution_pt2.rb                                                                                                                                              master
+# bundle exec ruby 01_solution_pt2.rb  0.64s user 0.13s system 97% cpu 0.790 total
+# ~/m/a/r/01 ❯❯❯ time bundle exec ruby 01_solution_pt2.rb                                                                                                                                              master
+# bundle exec ruby 01_solution_pt2.rb  0.73s user 0.13s system 94% cpu 0.910 total
+# ~/m/a/r/01 ❯❯❯ time bundle exec ruby 01_solution_pt2.rb                                                                                                                                              master
+# bundle exec ruby 01_solution_pt2.rb  0.61s user 0.13s system 97% cpu 0.762 total
+# ~/m/a/r/01 ❯❯❯ time bundle exec ruby 01_solution_pt2.rb                                                                                                                                            master ✱
+# bundle exec ruby 01_solution_pt2.rb  0.61s user 0.12s system 94% cpu 0.773 total
